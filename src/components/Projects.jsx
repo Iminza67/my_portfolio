6eeb76756f697920325568923305e919ab89f0d5
Olivia Iminza Hamisi <153586399+Iminza67@users.noreply.github.com>
2025-04-23 10:24:32 +0200

    edited the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
